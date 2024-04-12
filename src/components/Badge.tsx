d7068d4f76e5a034f0ec1ebf37df73418a39cd66
vinoth <shanvinod4u@gmail.com>
2024-04-12 09:11:18 +0000

    Remove Brand

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       > */}
-        <span> Bingo Brain</span>
+      <span> Bingo Brain</span>
       {/* </a> */}
     </span>
   );
