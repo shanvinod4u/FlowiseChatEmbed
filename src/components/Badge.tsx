debfeabf0cf0c9260d70e7b240fc9bcf576ca2e5
vinoth <shanvinod4u@gmail.com>
2024-04-12 05:19:40 +0000

    rebrand

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -54,7 +54,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ 'font-weight': 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span> Flowise</span>
+        <span>Bingo Brain</span>
       </a>
     </span>
   );
